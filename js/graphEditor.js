c7cf931c737594e84c2cdf24f8dba0ce788fe567
xClune <aclune382@gmail.com>
2024-02-04 07:17:11 +0800

    Created point hover over indicator

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -6,6 +6,7 @@ class GraphEditor {
         this.ctx = this.canvas.getContext("2d");
 
         this.selected = null;
+        this.hovered = null;
 
         this.#addEventListeners();
     }
@@ -14,13 +15,25 @@ class GraphEditor {
     #addEventListeners() {
         this.canvas.addEventListener("mousedown", (evt) => {
             const mouse = new Point(evt.offsetX, evt.offsetY);
+            this.hovered = getNearestPoint(mouse, this.graph.points, 12);
+            if (this.hovered) {
+                this.selected = this.hovered;
+                return;
+            }
             this.graph.addPoint(mouse);
             this.selected = mouse;
+        });
+        this.canvas.addEventListener("mousemove", (evt) => {
+            const mouse = new Point(evt.offsetX, evt.offsetY);
+            this.hovered = getNearestPoint(mouse, this.graph.points, 12);
         });
     }
 
     display() {
         this.graph.draw(this.ctx);
+        if (this.hovered) {
+            this.hovered.draw(this.ctx, { fill: true});
+        }
         if (this.selected) {
             this.selected.draw(this.ctx, { outline: true});
         }
