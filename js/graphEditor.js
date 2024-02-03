87a14642147e5e05310a74ef2529fd14ddee0042
xClune <aclune382@gmail.com>
2024-02-03 19:09:27 +0800

    Added graph editor class and method to generate point on click.

---
+++ js/graphEditor.js
@@ -0,0 +1,28 @@
+class GraphEditor {
+    constructor(canvas, graph) {
+        this.canvas = canvas;
+        this.graph = graph;
+
+        this.ctx = this.canvas.getContext("2d");
+
+        this.selected = null;
+
+        this.#addEventListeners();
+    }
+
+
+    #addEventListeners() {
+        this.canvas.addEventListener("mousedown", (evt) => {
+            const mouse = new Point(evt.offsetX, evt.offsetY);
+            this.graph.addPoint(mouse);
+            this.selected = mouse;
+        });
+    }
+
+    display() {
+        this.graph.draw(this.ctx);
+        if (this.selected) {
+            this.selected.draw(this.ctx, { outline: true});
+        }
+    }
+}
