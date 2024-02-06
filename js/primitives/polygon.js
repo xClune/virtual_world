2556a7048804035ed88fcabbab5b96947bc22c66
xClune <aclune382@gmail.com>
2024-02-06 15:37:06 +0800

    Created World class.
    Implemented function to display intersections between two envelopes.

--- js/primitives/polygon.js
+++ js/primitives/polygon.js
@@ -1,6 +1,34 @@
 class Polygon {
     constructor(points) {
         this.points = points;
+        this.segments = [];
+        for (let i = 1; i <= points.length; i++) {
+            this.segments.push(
+                new Segment(points[i-1], points[i % points.length]) // using the % operator means that the for loop will loop back to segments[0] at the final iteration.
+            );
+        }
+    }
+
+    static break(poly1, poly2) {
+        const segs1 = poly1.segments;
+        const segs2 = poly2.segments;
+        const intersections = [];
+        for (let i = 0; i < segs1.length; i++) {
+           for (let j = 0; j < segs2.length; j++) {
+            const int = getIntersection(
+                segs1[i].p1, 
+                segs1[i].p2, 
+                segs2[j].p1, 
+                segs2[j].p2)
+            ; 
+
+            if (int && int.offset != 1 && int.offset != 0) {
+                const point = new Point(int.x, int.y);
+                intersections.push(point);
+               } 
+           } 
+        }
+        return intersections;
     }
 
     draw(ctx, { stroke = "blue", lineWidth = 2, fill = "rgba(0,0,255,0.3)" } = {}) {
